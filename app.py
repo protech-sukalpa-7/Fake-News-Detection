ad932c3dd253977e0859c73aaf5539186b200e6b
protech-sukalpa-7 <mannasukalpa@gmail.com>
2025-10-09 09:44:39 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,16 +4,19 @@ import joblib
 vectorizer = joblib.load("vectorizer.jb")
 model = joblib.load("lr_model.jb")
 
-news_input = st.text_area("News Article:","")
+st.title("Fake News Detector")
+st.write("Enter a News Article below to check whether it is Fake or Real. ")
+
+inputn = st.text_area("News Article:","")
 
 if st.button("Check News"):
-    if news_input.strip():
-        transform_input = vectorizer.transform([news_input])
+    if inputn.strip():
+        transform_input = vectorizer.transform([inputn])
         prediction = model.predict(transform_input)
-        
-        if prediction[0]==1:
-            st.success("The News is real...")
+
+        if prediction[0] == 1:
+            st.success("The News is Real! ")
         else:
-            st.error("The News is fake...")
+            st.error("The News is Fake! ")
     else:
-        st.warning("Please Enter some text to analyze...")
+        st.warning("Please enter some text to Analyze. ") 
